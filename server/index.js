d2211e07381cf321423073f8fedd8ff2e1274976
masaokosan@gmail.com <katu2japan@gmail.com>
2026-08-15 23:49:58 +0900

    Minimize Stripe checkout permissions

--- server/index.js
+++ server/index.js
@@ -43,8 +43,6 @@ app.post('/api/create-checkout-session', async (req, res) => {
     const session = await stripe.checkout.sessions.create({
       mode: 'payment',
       integration_identifier: 'nightcompass_mtzqkavu',
-      customer_creation: 'always',
-      allow_promotion_codes: true,
       line_items: [
         {
           quantity: 1,
